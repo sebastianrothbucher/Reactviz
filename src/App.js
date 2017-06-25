7b79649cb914b166b91b1a6c6cc7832cd1ba9baf
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2017-06-25 16:16:29 +0200

    Refactor calc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
-let deg2Point=(deg) => (Math.round(250+200*Math.cos(deg*2*Math.PI/360))+" "+Math.round(250-200*Math.sin(deg*2*Math.PI/360)));
+const size=500;
+const middle=Math.round(size/2);
+const radius=Math.round(size/2-10);
+const bend=radius*4;
+
+let deg2Point=(deg) => (Math.round(middle+radius*Math.cos(deg*2*Math.PI/360))+" "+Math.round(middle-radius*Math.sin(deg*2*Math.PI/360)));
 
 class App extends Component {
   render() {
     return (
       <div>
-        <svg width="500" height="500">
-          <circle cx="250" cy="250" r="200" stroke="blue" stroke-width="1" fill="none"></circle>
-          <path d={"M "+deg2Point(10)+" A 200 200 0 0 0 "+deg2Point(20)+" A 800 800 0 0 1 "+deg2Point(180)+" A 200 200 0 0 0 "+deg2Point(195)} fill="grey" />
-          <path d={"M "+deg2Point(80)+" A 200 200 0 0 0 "+deg2Point(100)+" A 800 800 0 0 1 "+deg2Point(200)+" A 200 200 0 0 0 "+deg2Point(210)} fill="pink" />
+        <svg width={size} height={size}>
+          <circle cx={middle} cy={middle} r={radius} stroke="blue" stroke-width="1" fill="none"></circle>
+          <path d={"M "+deg2Point(10)+" A "+radius+" "+radius+" 0 0 0 "+deg2Point(20)+" A "+bend+" "+bend+" 0 0 1 "+deg2Point(180)+" A "+radius+" "+radius+" 0 0 0 "+deg2Point(195)} fill="grey" />
+          <path d={"M "+deg2Point(80)+" A "+radius+" "+radius+" 0 0 0 "+deg2Point(100)+" A "+bend+" "+bend+" 0 0 1 "+deg2Point(200)+" A "+radius+" "+radius+" 0 0 0 "+deg2Point(210)} fill="pink" />
         </svg>
       </div>
     );
